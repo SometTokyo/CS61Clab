fe27cf948c6ded809e67c7ef348ef1015729ee0b
somet <qianyuyouzhisan@gmail.com>
2023-08-13 09:25:56 +0900

    Finished

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -5,6 +5,15 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    uint16_t X0 = 1&(*reg>>0);
+    uint16_t X2 = 1&(*reg>>2); 
+    uint16_t X3 = 1&(*reg>>3); 
+    uint16_t X5 = 1&(*reg>>5);
+    uint16_t XOR1 = X0^X2;
+    uint16_t XOR2 = XOR1^X3; 
+    uint16_t XOR3 = XOR2^X5;
+    *reg >>= 1;
+    *reg = *reg + (XOR3<<15);
+       	/* YOUR CODE HERE */
 }
 
